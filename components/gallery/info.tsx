f64af008418f60f2b5c69e434390941527a8c3e3
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-02-27 18:36:55 +0300

    ınfo

--- components/gallery/info.tsx
+++ components/gallery/info.tsx
@@ -1,12 +1,56 @@
 import { Product } from '@/type'
 import React from 'react'
+import CurrencyTL from '../currency'
+import { Button } from '../ui/button'
+import { ShoppingBasket } from 'lucide-react'
 interface InfoProps{
     data:Product
 }
 
 const Info = ({data}:InfoProps) => {
   return (
-    <div>Info</div>
+    <div>
+        <h1 className='text-3xl font-bold'>{data.name}</h1>
+        <div>
+            <p className='text-2xl text-red-600'>
+                <CurrencyTL value={data?.price}></CurrencyTL>
+            </p>
+        </div>
+        <hr className='my-4'></hr>
+
+        <div className='flex flex-col gap-3'>
+            <div className='flex items-center'>
+                <h3 className='font-bold'>Size :</h3>
+                <div>
+                    {data?.size?.value}
+                </div>
+
+            </div>
+
+            <div className='flex items-center'>
+                <h3 className='font-bold'>Color :</h3>
+                <div className='h-6 w-6 rounded-full border-gray-600'
+                style={{backgroundColor :data?.color?.value }}>
+                </div>
+
+            </div>
+
+            <div className='mt-10 flex items-center gap-2'>
+                <Button  variant="destructive">
+                    <ShoppingBasket size={20} className='mr-2' />    Add to cart
+              
+                
+                </Button>
+
+
+
+            </div>
+
+
+        </div>
+
+
+    </div>
   )
 }
 
